c3522290ff409d89c393c7242af662ca66f4a4ed
MatGolzio <matheusgolzio09@gmail.com>
2022-03-12 11:14:02 -0300

    'connected'

---
+++ main.py
@@ -0,0 +1,87 @@
+from time import sleep
+
+def line():
+    print('~~'*30)
+
+def recognition(e):
+    with open('info/atoms.txt','r') as f:
+        lines = f.readlines()
+        for i in lines:
+            linesplit = i.split()
+            if (f'{e};' == linesplit[2]):
+                print(f'Name: {linesplit[0]} | Letter: {linesplit[1]} | Atomic Number: {linesplit[2]}')
+
+
+def distribution(e):
+    recognition(e)
+    line()
+    kv = 0
+    lv = 0
+    mv = 0
+    nv = 0
+    ov = 0
+    pv = 0
+    qv = 0
+
+    if e == 0:
+        print('There are no electrons to distribute')
+        line()
+    
+    elif e > 118:
+        print('There is no discovered atoms that has more than 118 protons and electrons')
+        line()
+
+    elif e == 1:
+        kv = 1
+    
+    elif e > 0:
+        k = e - 2
+        kv = e - k
+        if (k >= 8):
+            l = k - 8
+            lv = k - l
+            if (l >= 18):
+                m = l - 18
+                mv = l - m
+                if (m >= 32):
+                    n = m - 32
+                    nv = m - n
+                    if (n >= 32):
+                        o = n - 32
+                        ov = n - o
+                        if (o >= 18):
+                            p = o - 18
+                            pv = o - p
+                            if (p >= 8):
+                                q = p - 8
+                                qv = p - q
+                            elif (p < 8 and p != 0):
+                                qv = p
+
+                        elif (o < 18 and o != 0):
+                            pv = o
+                    elif (n < 32 and n != 0):
+                        ov = n
+                elif (m < 32 and m != 0):
+                    nv = m
+            
+            elif (l < 18 and l != 0):
+                mv = l
+        
+        elif (k < 8 and k != 0):
+            lv = k
+    
+    print(f'K layer = {kv}')
+    print(f'L layer = {lv}')
+    print(f'M layer = {mv}')
+    print(f'N layer = {nv}')
+    print(f'O layer = {ov}')
+    print(f'P layer = {pv}')
+    print(f'Q layer = {qv}')
+     
+        
+line()
+en = int(input('Type a number of electrons to distribute: '))
+line()
+distribution(en)
+sleep(5)
